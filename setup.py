7e39807c14247177dff0904e0f13a27d48f393e8
Isha Anand <ia5qk@virginia.edu>
2024-03-27 00:01:28 -0400

    update

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name= 'Demo',
+    version= '0.1',
+    description= 'demo package creation',
+    url= 'https://github.com/ia08/ClassworkM09',
+    author= 'Isha Anand',
+    author_email='ia5qk@virginia.edu',
+    license= 'MIT',
+    package= ['demo']
+)
